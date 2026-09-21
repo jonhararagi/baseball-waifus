43a11ec67237f85818c8e17a734f5a166cc39729
jonhararagi <128826150+jonhararagi@users.noreply.github.com>
2026-09-21 01:52:54 -0300

    Refine pitch data with auditable zone bias

--- game/baseball/pitch.gd
+++ game/baseball/pitch.gd
@@ -8,6 +8,7 @@ var name := ""
 var speed := 1.0
 var difficulty := 0.25
 var break_amount := 0.0
+var zone_bias := 0.86
 
 static func create(t: Type) -> Pitch:
 	var p := Pitch.new()
@@ -17,14 +18,17 @@ static func create(t: Type) -> Pitch:
 			p.name = "Fastball"
 			p.speed = 1.35
 			p.difficulty = 0.25
+			p.zone_bias = 0.90
 		Type.CURVE:
 			p.name = "Curve"
 			p.speed = 0.95
 			p.difficulty = 0.42
 			p.break_amount = 0.16
+			p.zone_bias = 0.82
 		Type.SPECIAL:
 			p.name = "Special"
 			p.speed = 1.10
 			p.difficulty = 0.52
 			p.break_amount = 0.08
+			p.zone_bias = 0.78
 	return p
